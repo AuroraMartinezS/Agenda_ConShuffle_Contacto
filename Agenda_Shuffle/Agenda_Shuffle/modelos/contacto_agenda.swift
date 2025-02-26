051b6b3637369dcf2ce10ccafbf54e21c47b46d5
alumno <alumno@PC-V202-10.uacj.mx>
2025-02-26 10:49:52 -0700

    [AGREGA]  un modelo de ocntacto para agregar informacion

---
+++ Agenda_Shuffle/Agenda_Shuffle/modelos/contacto_agenda.swift
@@ -0,0 +1,30 @@
+//
+//  contacto_agenda.swift
+//  Agenda_Shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+
+struct ContactoAgenda: Identifiable {
+    
+    var nombre: String
+    var telefono: String
+
+    var id: String { nombre }
+    
+}
+
+/*
+var nombre = "Pepito Veráz"
+
+var nombre: set{
+    let nombre_validado = filtrar_palabras(value):
+    _nombre = nombre_validado
+} get{
+    return _nombre.split(" ")[0]
+}
+
+*/
