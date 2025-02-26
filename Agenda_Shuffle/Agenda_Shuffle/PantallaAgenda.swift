b024ab17f0e850efc64985f27b293740823a8972
alumno <alumno@PC-V202-10.uacj.mx>
2025-02-26 10:51:19 -0700

    [AGREGA] cambios de modelos para contactos

--- Agenda_Shuffle/Agenda_Shuffle/PantallaAgenda.swift
+++ Agenda_Shuffle/Agenda_Shuffle/PantallaAgenda.swift
@@ -7,16 +7,28 @@
 
 import SwiftUI
 
+
+let contactos = [
+    ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987"),
+    ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987"),
+    ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987"),
+    ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987")
+]
+
 struct PantallaAgenda: View {
     var body: some View {
         //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         List {
             VStack {
-                ForEach(0...45, id: \.self){ _ in
+                /*ForEach(0...45, id: \.self){ _ in
                     Contacto_Preview()
                 }
-            }
-            .background(Color.cyan)
+            }*/
+                /*ForEach(contactos) { contacto in
+                    Text("\(contacto.nombre)")
+                    
+                }*/
+            //.background(Color.cyan)
         }
         
         
