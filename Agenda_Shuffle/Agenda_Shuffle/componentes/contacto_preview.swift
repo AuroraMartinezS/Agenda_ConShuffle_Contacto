b8e3aa42add8e5ebcce810da1eb8394af0cf3a42
alumno <alumno@PC-V202-10.uacj.mx>
2025-02-26 10:13:59 -0700

    [AGREGA] componente para la vista de contacto prevista

---
+++ Agenda_Shuffle/Agenda_Shuffle/componentes/contacto_preview.swift
@@ -0,0 +1,23 @@
+//
+//  contacto_preview.swift
+//  Agenda_Shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct Contacto_Preview: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "globe")
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        
+    }
+}
+
+#Preview {
+    Contacto_Preview()
+}
