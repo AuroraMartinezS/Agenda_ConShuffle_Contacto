af880a8b06a8a084835ac8f673e95fff2f583595
alumno <alumno@PC-V202-10.uacj.mx>
2025-02-28 10:41:17 -0700

    [AGREGAR] Pantalla con múltiples contactos y función al hacer click

--- Agenda_Shuffle/Agenda_Shuffle/PantallaAgenda.swift
+++ Agenda_Shuffle/Agenda_Shuffle/PantallaAgenda.swift
@@ -10,27 +10,37 @@ import SwiftUI
 
 let contactos = [
     ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987"),
-    ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987"),
-    ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987"),
-    ContactoAgenda(nombre: "Marisol", telefono: "656 098 0987")
+    ContactoAgenda(nombre: "Ana", telefono: "656 098 0987"),
+    ContactoAgenda(nombre: "Juan", telefono: "656 098 0987"),
+    ContactoAgenda(nombre: "Daniel", telefono: "656 098 0987"),
+    ContactoAgenda(nombre: "Sofia", telefono: "656 098 3456"),
+    ContactoAgenda(nombre: "Pepe", telefono: "656 123 3456"),
+    ContactoAgenda(nombre: "Karen", telefono: "656 345 1234")
 ]
 
 struct PantallaAgenda: View {
+    var largo = UIScreen.main.bounds.width
+    var ancho = UIScreen.main.bounds.height
+    
+    
     var body: some View {
-        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        List {
-            VStack {
+        ScrollView {
+            VStack(spacing: 10) {
                 /*ForEach(0...45, id: \.self){ _ in
-                    Contacto_Preview()
-                }
-            }*/
-                /*ForEach(contactos) { contacto in
-                    Text("\(contacto.nombre)")
+                 Contacto_Preview()
+                 }
+                 }*/
+                ForEach(contactos) { contacto in
+                    //Text("\(contacto.nombre)")
+                    ContactoPrevista(contacto_a_mostrar: contacto, al_pulsar: {
+                        print("Te envía un saludo \(contacto.nombre) desde la pantalla de agenda")
+                    })
                     
-                }*/
-            //.background(Color.cyan)
+                }
+            }
+            .background(Color.cyan)
+            .frame(width: largo, alignment: Alignment.center)
         }
-        
         
     }
 }
--- Agenda_Shuffle/Agenda_Shuffle/componentes/contacto_preview.swift
+++ Agenda_Shuffle/Agenda_Shuffle/componentes/contacto_preview.swift
@@ -7,17 +7,49 @@
 
 import SwiftUI
 
-struct Contacto_Preview: View {
+let contacto_prueba = ContactoAgenda(nombre: "sdfsdfsdfdsf", telefono: "09876543")
+
+struct ContactoPrevista: View {
+    var contacto_a_mostrar: ContactoAgenda
+    var al_pulsar: () -> Void = {print("No se ha implementado nada")}
+    
+    let esquinas_redondeadas = CGSize(width: 15, height: 15)
+    
     var body: some View {
-        HStack{
-            Image(systemName: "globe")
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: VerticalAlignment.center){
+            
+            Spacer()
+            
+            VStack{
+                Image("Imagen")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 75, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerSize: esquinas_redondeadas))
+            }.background(Color.white)
+            
+            Spacer()
+            
+            VStack(alignment: HorizontalAlignment.leading){
+                Text(contacto_a_mostrar.nombre)
+                Text(contacto_a_mostrar.telefono)
+            }
+            .frame(width: 150)
+            .background(Color.gray)
+            
+            
+            Spacer()
+        }
+        .frame(width: 250)
+        .background(Color.red)
+        .onTapGesture {
+            al_pulsar()
         }
         
     }
+    
 }
 
 #Preview {
-    Contacto_Preview()
+    ContactoPrevista(contacto_a_mostrar: contacto_prueba)
 }
